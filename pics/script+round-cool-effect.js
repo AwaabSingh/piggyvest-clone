99fc9f4b0e34eb1483bac2c4126c2e476ea2da75
ajaojnr5 <abdussamadajao@gmail.com>
2020-05-06 17:45:00 +0100

    clone

---
+++ pics/script+round-cool-effect.js
@@ -0,0 +1,8 @@
+// alert ('hello');
+function shownav() {
+	document.getElementById('mobilenav').style.display="block";
+	// document.getElementById('blur').style.opacity="0.3";
+}
+function closenav() {
+	document.getElementById('mobilenav').style.display="none";
+}
